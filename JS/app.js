f6f2782acd94b37ffd75bd3b0dd0c7e04a1915d6
Himaloy Datta. (Himu) <121860658+Himaloy989@users.noreply.github.com>
2023-04-08 14:00:25 +0600

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,24 @@
+let input = document.getElementById('search')
+let button = document.getElementById('glass')
+// let button = document.getElementById('cross')
+
+button.addEventListener('click', function () {
+    input.classList.add('show')
+})
+// button.addEventListener('click', function () {
+//     input.classList.remove('remove')
+// })
+
+// document.getElementById('cross').addEventListener('click', function () {
+//     document.getElementById
+// })
+
+$(function (){
+    'use strict'
+
+
+  
+})
+$(window).on('load', function(){
+    $('#preloader').fadeOut(1000);
+})
